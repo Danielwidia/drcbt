cfdaf9ff9bb4979fffe69499be9deb3d8f072f5c
Danielwidia <96729309+Danielwidia@users.noreply.github.com>
2026-09-10 10:20:19 +0700

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -18,6 +18,14 @@ try {
     fs.readFileSync(path.join(__dirname, '../public/app.js'));
     fs.readFileSync(path.join(__dirname, '../public/style.css'));
     fs.readFileSync(path.join(__dirname, '../public/logo.png'));
+    fs.readFileSync(path.join(__dirname, '../js/core/app-state.js'));
+    fs.readFileSync(path.join(__dirname, '../js/core/db-sync.js'));
+    fs.readFileSync(path.join(__dirname, '../js/core/ui-helpers.js'));
+    fs.readFileSync(path.join(__dirname, '../js/utils/formatters.js'));
+    fs.readFileSync(path.join(__dirname, '../js/modules/admin.js'));
+    fs.readFileSync(path.join(__dirname, '../js/modules/guru.js'));
+    fs.readFileSync(path.join(__dirname, '../js/modules/quizz.js'));
+    fs.readFileSync(path.join(__dirname, '../js/modules/siswa.js'));
 } catch (e) {}
 
 const app = require('../server');
